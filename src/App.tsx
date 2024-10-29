f7329ac39aba610aada54d4a5659fb3e19dfe216
unknown <asim26k@gmail.com>
2024-10-30 04:57:04 +0530

    Added gradient background

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import ProductPage from "./pages/ProductPage";
 function App() {
   return (
     <div className="flex h-screen bg-gray-950 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-950 via-gray-700 opacity-80" />
+        <div className="absolute inset-0, backdrop-blur " />
+      </div>
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductPage />} />
