4618cf4a24012c478bbbfdd595f8b980ea8f3ea2
unknown <asim26k@gmail.com>
2024-10-31 04:02:03 +0530

    FEATURE : Complete Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import OverviewPage from "./pages/OverviewPage";
 import ProductPage from "./pages/ProductPage";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="absolute inset-0 bg-gradient-to-br from-gray-950 via-gray-700 opacity-80" />
         <div className="absolute inset-0, backdrop-blur " />
       </div>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductPage />} />
